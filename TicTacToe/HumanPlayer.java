86c17d18a1f49b8467cdb7a2b2d03fb3742642ae
JomavisTec <127136236+JomavisTec@users.noreply.github.com>
2023-10-24 22:04:33 -0700

    Add files via upload

---
+++ TicTacToe/HumanPlayer.java
@@ -0,0 +1,24 @@
+package TicTacToe;
+
+import java.util.*;
+
+class HumanPlayer {
+	private Scanner Leer = new Scanner(System.in);
+    private char letter;
+
+    public HumanPlayer(char letter) {
+        this.letter = letter;
+    }
+
+    public int humanMove(char[] state) {
+        while (true) {
+        	System.out.print("Ingresa el número de casilla (1-9): ");
+            //int square = Integer.parseInt(System.console().readLine());
+            int square = Leer.nextInt();
+            System.out.println();
+            if (state[square - 1] == '-') {
+                return square - 1;
+            }
+        }
+    }
+}
